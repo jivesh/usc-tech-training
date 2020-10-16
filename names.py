a5ec8bafb20f62acffbdf5f8bed2180551e222dd
Allard <AllardQuek@users.noreply.github.com>
2020-10-16 20:01:56 +0800

    added jivesh's name

--- names.py
+++ names.py
@@ -1,4 +1,5 @@
 # Example of a print statement you can add
-print("Hi, I'm Allard")
+print("Hi, I'm Jivesh")
 
 # Print out your name below! Use the same format as above: "Hi I'm {name}"
+print("Hi, I'm Allard")
