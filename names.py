0b0c73406bd29f0d70105f1afaf44103479324fd
jiveshm2000@gmail.com <jiveshm2000@gmail.com>
2020-10-16 20:28:36 +0800

    moved hello

--- names.py
+++ names.py
@@ -1,8 +1,7 @@
 # Example of a print statement you can add
 print("Hi, I'm Jivesh")
 
+print("Hello")
 # Print out your name below! Use the same format as above: "Hi I'm {name}"
 print ("Hi, I'm Justin")
 print("Hi, I'm Allard")
-print ("Hello")
-
